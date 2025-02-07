7d320ba447c37da8da607e833e66873b675d3c06
explodingPan <cheyshel98@gmail.com>
2025-02-07 10:28:45 -0600

    2/7/25 - adjusting styles, removing unneded spaces or classes, fixing tab focus of image popup for keyboard accessibility

--- web2.0/src/app/shared/image-popup/image-popup.component.ts
+++ web2.0/src/app/shared/image-popup/image-popup.component.ts
@@ -14,10 +14,6 @@ export class ImagePopupComponent {
 
   popup:boolean = false;
 
-
-  
-
-
   imgPopup(open = false) {
     this.popup = open;
     const body = document.getElementsByTagName('body')[0];
@@ -25,6 +21,37 @@ export class ImagePopupComponent {
       body.classList.add('rm-scroll');
     } else {
       body.classList.remove('rm-scroll');
+    }
+  }
+
+  initTrapFocus(e:any, id:string) {
+    return this.trapFocus(e, id);
+  }
+
+  trapFocus(e:any, modalId:string) {
+    const isTabPressed = e.key === `Tab` || e.keyCode === 9;
+  
+    if (!isTabPressed) {
+      return;
+    }
+    const focusableElements = `button, input, select, textarea, iframe, [tabindex]:not([tabindex="-1"])`;
+    const modal = document.getElementById(modalId) as HTMLElement;
+  
+    // get focusable elements in modal
+    const firstFocusableElement = modal.querySelectorAll(focusableElements)[0] as HTMLElement;
+    const focusableContent = modal.querySelectorAll(focusableElements);
+    const lastFocusableElement = focusableContent[focusableContent.length - 1] as HTMLElement;
+
+    if (e.key == 'Shift') {
+      if (document.activeElement === firstFocusableElement) {
+        lastFocusableElement.focus();
+        e.preventDefault();
+      }
+    } else if (document.activeElement === lastFocusableElement) {
+      firstFocusableElement.focus();
+      e.preventDefault();
+    } else if (e.key == 'Enter') {
+      return;
     }
   }
 
