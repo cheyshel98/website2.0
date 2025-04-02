a506d400699c4afd4d07864a396bbfef3a2f1cdf
explodingPan <cheyshel98@gmail.com>
2025-04-02 15:17:06 -0500

    4/2/24 - Restore scroll to top when navigating to new page

--- web2.0/src/main.ts
+++ web2.0/src/main.ts
@@ -1,6 +1,21 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { InMemoryScrollingFeature, InMemoryScrollingOptions, provideRouter, withInMemoryScrolling } from '@angular/router';
+import { routes } from './app/app.routes';
 
 bootstrapApplication(AppComponent, appConfig,)
   .catch((err) => console.error(err));
+
+//solution from Ole Ersoy on Medium
+//https://fireflysemantics.medium.com/turning-on-scrolltop-restoration-for-the-standalone-angular-router-afd98bd8defd
+
+const scrollConfig: InMemoryScrollingOptions = {
+  scrollPositionRestoration: 'top',
+  anchorScrolling: 'enabled',
+};
+
+const inMemoryScrollingFeature: InMemoryScrollingFeature = withInMemoryScrolling(scrollConfig);
+bootstrapApplication(AppComponent, {
+  providers: [provideRouter(routes, inMemoryScrollingFeature)],
+});
